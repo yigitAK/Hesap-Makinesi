7b3eb1bb394c6f49489aa459722a4356e236d83a
yigitAK <y.ak@ebebek.com>
2022-08-02 17:16:35 +0300

    toplama işlemi eklendi

--- src/com/ebebek/Main.java
+++ src/com/ebebek/Main.java
@@ -1,8 +1,33 @@
 package com.ebebek;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int number1, number2, operation;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("İki tamsayı giriniz: ");
+
+        System.out.println("Sayı 1: ");
+        number1 = input.nextInt();
+
+        System.out.println("Sayı 2: ");
+        number2 = input.nextInt();
+
+        System.out.println("\nToplama (1)");
+
+        System.out.println("Yapmak istediğiniz işlem için uygun sayıyı giriniz:");
+        operation = input.nextInt();
+
+        switch (operation){
+            case 1:
+                System.out.println(number1+number2);
+
+        }
+
     }
 }
