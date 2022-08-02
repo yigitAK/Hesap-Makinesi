75e852042599596f3a13cf21398e57a85ed9d451
Talha Durmus <kule_galata@hotmail.com>
2022-08-02 17:35:07 +0300

    Hesap makinesi üzerinde işlem düzenlemeleri yapıldı

--- src/com/ebebek/Main.java
+++ src/com/ebebek/Main.java
@@ -6,30 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("*** Hesap Makinesi ***\n");
+
         int number1, number2, operation;
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("İki tamsayı giriniz: ");
 
-        System.out.println("Sayı 1: ");
+        System.out.print("Sayı 1: ");
         number1 = input.nextInt();
 
-        System.out.println("Sayı 2: ");
+        System.out.print("Sayı 2: ");
         number2 = input.nextInt();
 
         System.out.println("\nToplama (1)");
         System.out.println("Çıkarma (2)");
+        System.out.println("Çarpma (3)");
+        System.out.println("Bölme (4)");
 
-        System.out.println("Yapmak istediğiniz işlem için uygun sayıyı giriniz:");
+        System.out.print("Yapmak istediğiniz işlem için uygun sayıyı giriniz: ");
         operation = input.nextInt();
 
         switch (operation){
             case 1:
-                System.out.println(number1+number2);
-
+                System.out.println("\nİşlem sonucu: "+number1+" + "+number2+" = "+(number1+number2));
+                break;
             case 2:
-                System.out.println(number1-number2);
+                System.out.println("\nİşlem sonucu: "+number1+" - "+number2+" = "+(number1-number2));
+                break;
+            case 3:
+                System.out.println("\nİşlem sonucu: "+number1+" * "+number2+" = "+(number1*number2));
+                break;
+            case 4:
+                System.out.println("\nİşlem sonucu: "+number1+" / "+number2+" = "+(number1/number2));
+                break;
         }
 
     }
